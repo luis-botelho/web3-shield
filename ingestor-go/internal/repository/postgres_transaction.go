48f007b9495c9d4eded9db62f7534c64ab12e52e
Luis Fellype Botelho <79022593+luis-botelho@users.noreply.github.com>
2026-09-21 21:44:34 -0300

    feat(ingestor): captura remetente das transações via assinatura ECDSA

--- ingestor-go/internal/repository/postgres_transaction.go
+++ ingestor-go/internal/repository/postgres_transaction.go
@@ -22,11 +22,11 @@ func NewPostgresTransactionRepository(db *sql.DB) *PostgresTransactionRepository
 // Save insere a transação, ignorando hashes já persistidos.
 func (r *PostgresTransactionRepository) Save(tx domain.Transaction) error {
 	query := `
-	INSERT INTO raw_transactions (tx_hash, to_address, function_signature, block_number, block_hash) 
-	VALUES ($1, $2, $3, $4, $5) 
+	INSERT INTO raw_transactions (tx_hash, from_address, to_address, function_signature, block_number, block_hash) 
+	VALUES ($1, $2, $3, $4, $5, $6) 
 	ON CONFLICT (tx_hash) DO NOTHING;`
 
-	_, err := r.db.Exec(query, tx.Hash, tx.ToAddress, tx.FunctionSignature, tx.BlockNumber, tx.BlockHash)
+	_, err := r.db.Exec(query, tx.Hash, tx.FromAddress, tx.ToAddress, tx.FunctionSignature, tx.BlockNumber, tx.BlockHash)
 	if err != nil {
 		return fmt.Errorf("erro ao salvar transação no banco: %w", err)
 	}
